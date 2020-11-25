ccff5e47fe28392c88944b5cd19340aefb1f086e
jian <850355699@qq.com>
2020-11-25 22:55:16 +0800

    first come first service of Algorithm.

--- 调度算法/FCFS.py
+++ 调度算法/FCFS.py
@@ -3,6 +3,7 @@ class Node(object):
         self.arriveTime = 0  # 到达时间
         self.serviceTime = 0  # 要求服务时间
         self.startTime = 0  # 开始运行时间
+        self.endTime = 0  # 结束运行时间
         self.waitTime = 0  # 周转时间
         self.averageTime = 0  # 带权周转时间
 
@@ -17,19 +18,32 @@ def readTask(pid):
 
 
 def runTask():
-    pass
+    global endTime
+    for i in range(len(Nodes)):
+        pid = i + 1
+        if i == 0:
+            startTime = Nodes[i].arriveTime
+            endTime = Nodes[i].arriveTime + Nodes[i].serviceTime
+        else:
+            startTime = endTime + 1
+            endTime += (Nodes[i].serviceTime + 1)
+        waitTime = endTime - Nodes[i].arriveTime  # 周转时间= 结束运行时间-到达时间
+        averageTime = waitTime / Nodes[i].serviceTime  # 带权周转时间=周转时间/服务时间
+        printTask(pid, startTime, waitTime, averageTime, endTime)
 
 
-def printTask():
-    pid = 1
-    for node in Nodes:
-        print('进程{}的到达时间:{}'.format(pid, node.arriveTime))
-        print('进程{}的要求服务时间:{}'.format(pid, node.serviceTime))
-        print('进程{}的开始运行时间:{}'.format(pid, node.startTime))
-        print('进程{}的周转时间时间:{}'.format(pid, node.waitTime))
-        print('进程{}的带权周转时间:{}'.format(pid, node.averageTime))
-        print('*' * 20)
-        pid += 1
+def printTask(pid, startTime, waitTime, averageTime, endTime):
+    Nodes[pid - 1].startTime = startTime
+    Nodes[pid - 1].waitTime = waitTime
+    Nodes[pid - 1].averageTime = averageTime
+    Nodes[pid - 1].endTime = endTime
+    print('进程{}的到达时间:{}'.format(pid, Nodes[pid - 1].arriveTime))
+    print('进程{}的要求服务时间:{}'.format(pid, Nodes[pid - 1].serviceTime))
+    print('进程{}的开始运行时间:{}'.format(pid, Nodes[pid - 1].startTime))
+    print('进程{}的结束运行时间:{}'.format(pid, Nodes[pid - 1].endTime))
+    print('进程{}的周转时间时间:{}'.format(pid, Nodes[pid - 1].waitTime))
+    print('进程{}的带权周转时间:{}'.format(pid, Nodes[pid - 1].averageTime))
+    print('*' * 30)
 
 
 def main():
@@ -38,7 +52,6 @@ def main():
     for pid in range(1, n + 1):
         readTask(pid)
     runTask()
-    printTask()
 
 
 if __name__ == '__main__':
